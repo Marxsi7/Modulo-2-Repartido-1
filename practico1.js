533bfee042086209486afd526d2222fa6e054931
Marxsi7 <89942553+Marxsi7@users.noreply.github.com>
2021-10-18 17:22:45 -0300

    lunes 18/10
    lunes 18/10

---
+++ practico1.js
@@ -0,0 +1,51 @@
+/*var numero1 = prompt ("ingrese su número");
+var numero2 = prompt ("ingrese  su otro número");
+var num1 = parseInt(numero1);
+var num2 = parseInt(numero2);
+
+var suma = num1 + num2; 
+
+alert("El resultado de la suma es " + suma);
+
+document.write("El resultado de la suma es " + suma)
+
+var numero1 = prompt ("ingrese su número");
+var numero2 = prompt ("ingrese  su otro número");
+var num1 = parseInt(numero1);
+var num2 = parseInt(numero2);
+
+var multiplicacion = num1 * num2; 
+
+alert(multiplicacion);
+
+document.write("El resultado de la multiplicacion es " * multiplicacion);
+*/
+
+var numero1 = prompt ("ingrese su número");
+var numero2 = prompt ("ingrese  su otro número");
+var numero3 = prompt ("ingrese su número");
+var numero4 = prompt ("ingrese su número");
+var numero5 = prompt ("ingrese su número");
+var num1 = parseInt(numero1);
+var num2 = parseInt(numero2);
+var num3 = parseInt(numero3);
+var num4 = parseInt(numero4);
+var num5 = parseInt(numero5);
+
+var promedio = parseFloat(num1)+parseFloat(num2)+parseFloat(num3)+parseFloat(num4)+parseFloat(num5) / 5
+alert(promedio);
+
+var numero1 = prompt ("ingrese su número");
+var numero2 = prompt ("ingrese  su otro número");
+var numero3 = prompt ("ingrese su número");
+var numero4 = prompt ("ingrese su número");
+var numero5 = prompt ("ingrese su número");
+var num1 = parseInt(numero1);
+var num2 = parseInt(numero2);
+var num3 = parseInt(numero3);
+var num4 = parseInt(numero4);
+var num5 = parseInt(numero5);
+
+var promedio = parseFloat(num1)+parseFloat(num2)+parseFloat(num3)+parseFloat(num4)+parseFloat(num5) / 5
+alert(promedio);
+
