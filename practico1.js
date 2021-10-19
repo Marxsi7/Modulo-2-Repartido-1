e8d70dc364d04bf444ac0a1a8b419c8478ffc41b
Marxsi7 <89942553+Marxsi7@users.noreply.github.com>
2021-10-19 17:04:13 -0300

    19/10 17:04
    17:04

--- practico1.js
+++ practico1.js
@@ -1,4 +1,5 @@
-/*var numero1 = prompt ("ingrese su número");
+/* 1 suma*/
+var numero1 = prompt ("ingrese su número");
 var numero2 = prompt ("ingrese  su otro número");
 var num1 = parseInt(numero1);
 var num2 = parseInt(numero2);
@@ -8,6 +9,8 @@ var suma = num1 + num2;
 alert("El resultado de la suma es " + suma);
 
 document.write("El resultado de la suma es " + suma)
+
+/* 2 multiplicacion */
 
 var numero1 = prompt ("ingrese su número");
 var numero2 = prompt ("ingrese  su otro número");
@@ -19,7 +22,8 @@ var multiplicacion = num1 * num2;
 alert(multiplicacion);
 
 document.write("El resultado de la multiplicacion es " * multiplicacion);
-*/
+
+// /*promedio*/
 
 var numero1 = prompt ("ingrese su número");
 var numero2 = prompt ("ingrese  su otro número");
@@ -32,20 +36,36 @@ var num3 = parseInt(numero3);
 var num4 = parseInt(numero4);
 var num5 = parseInt(numero5);
 
-var promedio = parseFloat(num1)+parseFloat(num2)+parseFloat(num3)+parseFloat(num4)+parseFloat(num5) / 5
-alert(promedio);
+ var promedio = (parseFloat(num1)+parseFloat(num2)+parseFloat(num3)+parseFloat(num4)+parseFloat(num5)) / 5
+ alert(promedio);
 
-var numero1 = prompt ("ingrese su número");
-var numero2 = prompt ("ingrese  su otro número");
-var numero3 = prompt ("ingrese su número");
-var numero4 = prompt ("ingrese su número");
-var numero5 = prompt ("ingrese su número");
+ /*perimetro*/
+var numero1 = prompt ("Ingrese lado A");
+ var numero2 = prompt ("ingrese  lado B");
+
 var num1 = parseInt(numero1);
 var num2 = parseInt(numero2);
-var num3 = parseInt(numero3);
-var num4 = parseInt(numero4);
-var num5 = parseInt(numero5);
 
-var promedio = parseFloat(num1)+parseFloat(num2)+parseFloat(num3)+parseFloat(num4)+parseFloat(num5) / 5
-alert(promedio);
+var perimetro = (2*num2)+(2*num1);
+alert (perimetro)
+
+/* area */
+ var numero1 = prompt ("Ingrese base del triangulo");
+var numero2 = prompt ("ingrese altura del trinagulo");
+
+ var num1 = parseInt(numero1);
+ var num2 = parseInt(numero2);
+
+ var area = (num1*num2)/2;
+ alert (area);
+ 
+/*Datos completos*/
+
+var nombre = prompt ("Ingrese su nombre");
+var apellido = prompt ("Ingrese su apellido");
+var edad = parseInt(prompt ("Ingrese su edad"));
+
+
+alert(nombre.toLowerCase() + "\n" + apellido.toUpperCase()  + "\n" +  edad); 
+
 
