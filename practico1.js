72827b6a3472301dbaf77d0654d430516aded504
Marxsi7 <89942553+Marxsi7@users.noreply.github.com>
2021-10-20 16:24:16 -0300

    16:24 20/10

--- practico1.js
+++ practico1.js
@@ -1,3 +1,5 @@
+
+
 /* 1 suma*/
 var numero1 = prompt ("ingrese su número");
 var numero2 = prompt ("ingrese  su otro número");
@@ -23,7 +25,7 @@ alert(multiplicacion);
 
 document.write("El resultado de la multiplicacion es " * multiplicacion);
 
-// /*promedio*/
+/*3 promedio*/
 
 var numero1 = prompt ("ingrese su número");
 var numero2 = prompt ("ingrese  su otro número");
@@ -36,12 +38,12 @@ var num3 = parseInt(numero3);
 var num4 = parseInt(numero4);
 var num5 = parseInt(numero5);
 
- var promedio = (parseFloat(num1)+parseFloat(num2)+parseFloat(num3)+parseFloat(num4)+parseFloat(num5)) / 5
- alert(promedio);
+var promedio = (parseFloat(num1)+parseFloat(num2)+parseFloat(num3)+parseFloat(num4)+parseFloat(num5)) / 5
+alert(promedio);
 
- /*perimetro*/
+/*  4  perimetro*/
 var numero1 = prompt ("Ingrese lado A");
- var numero2 = prompt ("ingrese  lado B");
+var numero2 = prompt ("ingrese  lado B");
 
 var num1 = parseInt(numero1);
 var num2 = parseInt(numero2);
@@ -49,23 +51,61 @@ var num2 = parseInt(numero2);
 var perimetro = (2*num2)+(2*num1);
 alert (perimetro)
 
-/* area */
- var numero1 = prompt ("Ingrese base del triangulo");
+/* 5  area */
+var numero1 = prompt ("Ingrese base del triangulo");
 var numero2 = prompt ("ingrese altura del trinagulo");
 
- var num1 = parseInt(numero1);
- var num2 = parseInt(numero2);
+var num1 = parseInt(numero1);
+var num2 = parseInt(numero2);
 
- var area = (num1*num2)/2;
- alert (area);
+var area = (num1*num2)/2;
+alert (area);
  
-/*Datos completos*/
+/*  6  Datos completos*/
 
 var nombre = prompt ("Ingrese su nombre");
 var apellido = prompt ("Ingrese su apellido");
 var edad = parseInt(prompt ("Ingrese su edad"));
 
+alert(nombre.toLowerCase() + "\n" + apellido.toUpperCase()  + "\n" +  edad);
 
-alert(nombre.toLowerCase() + "\n" + apellido.toUpperCase()  + "\n" +  edad); 
+ /*  7   conversión moneda */
+
+var pesos = prompt ("ingrese pesos uruguayos");
+var num1 = parseInt(pesos);
+
+var conversión = num1 * 0.023; 
+
+alert(conversión);
+
+document.write("El resultado de la suma es " + suma)
+
+/*    8  numero par ,impar */
+
+var par = prompt ("ingrese número");
+var num1 = parseInt(par);
+
+     if (num1 % 2 == 0)
+
+    {
+         document.write('El numero introducido es par');
+    }
+    else {document.write('El numero introducido es impar');}
+ alert ();
+
+/* 9   votantes*/
+var fechanacimiento = prompt ("ingrese año de nacimiento");
+var num1 = parseInt(fechanacimiento);
+if (fechanacimiento >= 18)
+        {
+            document.write('Puedes votar')
+        }
+        
+        else (fechanacimiento < 18) 
+        {
+            document.write('No puedes votar')
+        }
+
+
 
 
